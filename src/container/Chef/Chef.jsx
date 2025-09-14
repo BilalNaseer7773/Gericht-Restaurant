4f1cde8f913cac0ede79a8be08ec31932760cea1
BilalNaseer7773 <mbnaseer@mun.ca>
2025-09-14 17:01:34 -0230

    adding gallery to the gericht restaurant

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import { images } from '../../constants';
+
+import { SubHeading } from '../../components';
 
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt='chefs'/>
+    </div>
+
+    <div className='app__wrapper_info'>
+      <SubHeading title='Chefs World' />
+      <h2 className='headtext__cormorant'> What We Believe In.</h2>
+
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="quote" />
+        </div>
+        <p className='p__opensans'>We believe cooking is an art. Our chefs pour passion, creativity, and care into every plate, ensuring each meal is memorable and meaningful. We strive to create a warm, welcoming space where guests feel like family and the community comes together.</p>
+      </div>
+
+      <div className='app__chef-sign'>
+        <p>Kevin Luo</p>
+        <p className='p__opensans'>Chef & Founder</p>
+        <img src= {images.sign} alt='sign'/>
+      </div>
+    </div>
   </div>
 );
 
